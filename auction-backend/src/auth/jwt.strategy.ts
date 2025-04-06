b428453632048efa6f2729ae719c09d1e0897e8d
TuNguyennAnh <khosithuyminh@gmail.com>
2025-04-06 18:57:50 +0700

    Thêm mới

--- auction-backend/src/auth/jwt.strategy.ts
+++ auction-backend/src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'your_jwt_secret', // thay bằng biến môi trường thật nếu có
+      secretOrKey: process.env.JWT_SECRET || 'vietdaugia-secret', // thay bằng biến môi trường thật nếu có
     });
   }
 
