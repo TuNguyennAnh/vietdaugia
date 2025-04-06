0726f95b320d511b66b58832621ac845a4f13ee2
TuNguyennAnh <khosithuyminh@gmail.com>
2025-04-06 16:38:23 +0700

    Sửa đấu giá

--- auction-backend/src/main.ts
+++ auction-backend/src/main.ts
@@ -1,17 +1,23 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // ✅ Bật CORS đúng cách, cho phép tất cả origin (hoặc bạn có thể giới hạn sau)
   app.enableCors({
-    origin: '*', // cho phép mọi domain gọi API
+    origin: ['https://vietdaugia-frontend.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // nếu bạn dùng cookie / xác thực
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
